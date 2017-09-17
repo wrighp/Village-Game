2ba523d140092e49e7b41b5c6013560f45129c24
wrighp <wrightCoder@gmail.com>
2017-09-17 19:07:39 -0400

    :movie_camera: Camera deadzone without projection scaling
    Camera gets average position of targets and moves camera if it is
    outside of deadzone.

---
+++ Assets/Code/Camera/CameraDeadzone.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDeadzone : MonoBehaviour {
+	
+	public float deadzone; //Deadzone in either direction before moving
+	public List<Transform> targets;
+
+	//private float modifiedDeadzone;
+	bool selected;
+	void Awake(){
+		targets = new List<Transform>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(targets.Count == 0){
+			DrawBoundary();
+			return;
+		}
+
+		//Get average positions of all transforms
+		Vector3 averagePosition = new Vector3();
+		for (int i = 0, targetsCount = targets.Count; i < targetsCount; i++) {
+			averagePosition += targets [i].position;
+		}
+		averagePosition *= 1f/targets.Count;
+
+		//Get max and min x to set camera projection size if necessary
+		//
+		//
+
+		if(selected){
+			LineDebug.DrawRay(averagePosition, Vector3.up, Color.red);
+		}
+
+		//Push camera if average position moves outside of deadzone
+		float x = averagePosition.x - transform.position.x;
+		float distance = Mathf.Abs(x);
+		if(distance > deadzone){
+			float direction = Mathf.Sign(x);
+			Vector3 pos = transform.position;
+			pos.x = averagePosition.x - deadzone * direction;
+			transform.position = pos;
+		}
+
+		DrawBoundary();
+	}
+	void DrawBoundary(){
+		if(!selected){
+			return;
+		}
+		Color color = Color.red;
+		LineDebug.DrawRay(transform.position + new Vector3(deadzone, -5f , 1f), Vector3.up * 10f, color);
+		LineDebug.DrawRay(transform.position + new Vector3(-deadzone, -5f , 1f), Vector3.up * 10f, color);
+		selected = false;
+	}
+	void OnDrawGizmosSelected(){
+		selected = true;
+	}
+
+}
--- Assets/Code/Grid/DebugGrid.cs
+++ Assets/Code/Grid/DebugGrid.cs
@@ -25,34 +25,12 @@ public class DebugGrid : MonoBehaviour {
 		for(int i = 0; i < xSegments + 1; i++){
 			Vector3 v1 = positionOffset + new Vector3(xPart * (i), 0);
 			Vector2 v2 = positionOffset + new Vector3(xPart * (i + 1), height);
-			verts.Add(v1);
-			verts.Add(v2);
-			Debug.DrawLine(v1, v2, color, 0,true);
+			LineDebug.DrawLine(v1, v2, color);
 		}
 		for(int j = 0; j < ySegments + 1; j++){
 			Vector3 v1 = positionOffset + new Vector3(xPart * j / ySegments, yPart * j);
 			Vector3 v2 = positionOffset + new Vector3(width + xPart * j / ySegments, yPart * (j));
-			verts.Add(v1);
-			verts.Add(v2);
-			Debug.DrawLine(v1, v2,color, 0,true);
+			LineDebug.DrawLine(v1, v2, color);
 		}
-	}
-
-	void OnPostRender () {
-
-		GL.PushMatrix();
-		mat.SetPass(0);
-		mat.color = color;
-		//GL.LoadOrtho();
-		GL.Begin(GL.LINES);
-		GL.Color(color);
-
-		for (int i = 0, vertsCount = verts.Count; i < vertsCount; i++) {
-			GL.Vertex(verts [i]);
-		}
-
-		GL.End();
-		GL.PopMatrix();
-		verts.Clear();
 	}
 }
