677407da708a1e9276dd978fe22218a779f4f18e
John Steigerwald <johnstiggy@gmail.com>
2017-09-24 16:41:00 -0400

    ✨ Basis for Quest System Added
    - Quests Contain a basic choice system for the player to select from,
    each choice has further branching based on either resource requirements
    or by unweighted random rolls. All data for a quest can be edited via
    scriptable objects.
    
    - Currently the quest handler just runs a single branch as a proof of
    concept.

---
+++ Assets/Code/QuestHandler.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Reflection;
+
+public class QuestHandler : MonoBehaviour {
+
+    public QuestObject currentQuest;
+    SupplyData gameData;
+
+	// Use this for initialization
+	void Start () {
+        gameData = transform.gameObject.GetComponent<SupplyData>();
+        print(gameData.gameObject.name);
+        print(currentQuest.Description);
+        print(currentQuest.questDecisions[0].decisionDescription);
+        QuestResults(currentQuest.questDecisions[0]);
+	}
+
+    void QuestResults(Decision questDecision){
+        Branch selectedBranch;
+        if(questDecision.branchStyle == BranchStyle.Random) {
+            selectedBranch = UsefulExtensions.Pick<Branch>(questDecision.branches);
+        } else {
+            bool requirementsMet = true;
+            foreach (Requirement r in questDecision.requirements){
+                requirementsMet = requirementsMet & CompareValue(r.type.ToString(), r.amount);
+            }
+            if (requirementsMet){
+                selectedBranch = questDecision.branches[0];
+            } else {
+                selectedBranch = questDecision.branches[1];
+            }
+        }
+        foreach(Result r in selectedBranch.results){
+            ChangeValue(r.value.ToString(), r.quantity);
+        }
+        print(selectedBranch.Description);
+    }
+
+    public bool CompareValue(string type, int quantity){
+        FieldInfo value = gameData.GetType().GetField(type);
+        return ((int)value.GetValue(gameData)) >= quantity;
+    }
+
+    public void ChangeValue(string type, int quantity){
+        FieldInfo value = gameData.GetType().GetField(type);
+        value.SetValue(gameData, quantity + (int)value.GetValue(gameData));
+    }
+}
--- Assets/Code/ScriptableObjectsCode/QuestObject.cs
+++ Assets/Code/ScriptableObjectsCode/QuestObject.cs
@@ -1,7 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Playables;
+using System;
+using UnityEngine.Networking;
+
+public enum Choices { wood, stone, food, gold };
+public enum BranchStyle { Random, Requirement };
 
 [CreateAssetMenu(fileName = "New Quest", menuName = "ScriptableObject/QuestObject", order = 1)]
 public class QuestObject : ScriptableObject {
-	public string body = "";
+    public string QuestName= "";
+	public string Description = "";
+    public Decision[] questDecisions;
+}
+
+[Serializable]
+public class Decision{
+    public string decisionDescription = "";
+    public Branch[] branches;
+    public BranchStyle branchStyle;
+    public Requirement[] requirements;
+}
+
+[Serializable]
+public class Requirement{
+    public Choices type;
+    public int amount;
+}
+
+[Serializable]
+public class Branch{
+    public string Description = "";
+    public Result[] results;
+}
+
+[Serializable]
+public class Result{
+    public Choices value;
+    public int quantity;
 }
