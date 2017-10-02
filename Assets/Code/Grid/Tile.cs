8fdcc19a558bc9dce644805e3b7dea229055e7f1
wrighp <wrightCoder@gmail.com>
2017-10-02 17:34:07 -0400

    :sparkles: Tile unit interaction

---
+++ Assets/Code/Grid/Tile.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Tile : NetworkBehaviour {
+
+	CircleCollider2D circleCollider;
+	bool triggering;
+
+	public SyncListSquadUnit units = new SyncListSquadUnit();
+
+	void Awake(){
+		circleCollider = GetComponent<CircleCollider2D>();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Debug
+		//Scales Debug circle to the size of the actual circle drawn
+		float maxDimension = Mathf.Max(transform.lossyScale.x, transform.lossyScale.y);
+		Color c = new Color(0,0,.5f,.125f);
+		if(triggering){
+			c = new Color(1f,0,0 ,.125f);
+		}
+		PlayerDebug.DrawCircle(transform.position, circleCollider.radius * maxDimension, c);
+
+
+
+	}
+
+	void FixedUpdate(){
+		triggering = false;
+	}
+
+	void OnTriggerEnter2D(Collider2D collider){
+		
+	}
+
+
+	/// <summary>
+	/// Raises the trigger event, only Player layers can interact with tile triggers
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+	void OnTriggerStay2D(Collider2D collider){
+		triggering = true;
+		//Call function on the player unit controller, so that it may interact with this Tile
+		collider.GetComponent<PlayerUnitControl>().OnTileCollision(this);
+
+	}
+
+	void OnTriggerExit2D(Collider2D collider){
+		
+	}
+
+	[Command]
+	public void CmdAddUnit(SquadUnit unit){
+		units.Add(unit);
+		unit.follower.target = transform;
+		unit.follower.minDistance = 0;
+	}
+}
---
+++ Assets/Code/Units/PlayerUnitControl.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public struct SquadUnit{
+	public GameObject unit;
+	public FollowerMovement follower;
+	public SquadUnit(GameObject unit, FollowerMovement follower){
+		this.unit = unit;
+		this.follower = follower;
+	}
+
+	public static SquadUnit GameObjectToSquadUnit(GameObject go){
+		return new SquadUnit(go, go.GetComponent<FollowerMovement>());
+	}
+}
+public class SyncListSquadUnit : SyncListStruct<SquadUnit>{}
+
+public class PlayerUnitControl : NetworkBehaviour {
+
+	public GameObject TestUnit;
+	public float minFollowerDistance = 4f;
+	public SyncListSquadUnit squad = new SyncListSquadUnit();
+
+
+	void Awake(){
+		squad.Callback = OnSquadChanged;
+	}
+
+	public override void OnStartClient ()
+	{
+		base.OnStartClient ();
+
+	}
+	public override void OnStartLocalPlayer ()
+	{
+		base.OnStartLocalPlayer ();
+
+		CmdTestSpawnSquad();
+	}
+
+	/// <summary>
+	/// Tests a squad by spawning it in
+	/// </summary>
+	[Command]
+	void CmdTestSpawnSquad(){
+		int count = 3;
+		for(int i = 0; i < count; i++){
+			GameObject go = GameObject.Instantiate (TestUnit, transform.position, Quaternion.identity);
+			NetworkServer.Spawn(go);
+			SquadUnit su = SquadUnit.GameObjectToSquadUnit(go);
+			//Should be assigned at authority, in this case the server
+			CmdAddUnitToSquad(su);
+		}
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+	void CmdAddUnitToSquad(GameObject go){
+		CmdAddUnitToSquad(SquadUnit.GameObjectToSquadUnit(go));
+	}
+
+	[Command]
+	void CmdAddUnitToSquad(SquadUnit s){
+		s.follower.target = transform;
+		s.follower.minDistance = minFollowerDistance;
+		squad.Add(s);
+	}
+
+	void CmdRemoveUnitFromSquad(GameObject go){
+		CmdRemoveUnitFromSquad(SquadUnit.GameObjectToSquadUnit(go));
+	}
+
+	[Command]
+	void CmdRemoveUnitFromSquad(SquadUnit s){
+		squad.Remove(s);
+	}
+
+
+	void OnSquadChanged(SyncListSquadUnit.Operation op, int index){
+		//SyncListStruct<SquadUnit>.Operation.
+
+
+	}
+
+	/// <summary>
+	/// When Player interacts with a tile
+	/// </summary>
+	/// <param name="tile">Tile.</param>
+	//[Command]
+	void TileInteract(Tile tile){
+		if(tile.units.Count > 0){
+			//Add unit to squad, remove from tile
+			CmdAddUnitToSquad(tile.units[0]);
+			tile.units.Remove(tile.units[0]);
+		}
+		else{
+			//Remove unit from squad, add to tile
+			if(squad.Count > 0){
+				tile.CmdAddUnit(squad[0]);
+				CmdRemoveUnitFromSquad(squad[0]);
+			}
+		}
+
+	}
+
+	void LocalTileInteract(Tile tile){
+		if(isServer){
+			TileInteract(tile);
+		}
+	}
+
+
+	public void OnTileCollision(Tile tile){
+		if(isServer){
+			
+		}
+		if(isLocalPlayer){
+			if(Input.GetButtonDown("Interact")){
+				LocalTileInteract(tile);
+			}
+		}
+		if(isClient){
+			
+		}
+	}
+
+}
