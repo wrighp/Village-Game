7acc0d794d4d89ff69453360d8b10ad665356271
wrighp <wrightCoder@gmail.com>
2017-10-16 16:47:57 -0400

    :wrench: PolygonCollider2D mesh triangulator

---
+++ Assets/Code/Collider2DMesh.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Linq;
+using System;
+
+[RequireComponent(typeof(MeshFilter))]
+public class Collider2DMesh : MonoBehaviour {
+
+
+	public void TriangulateMesh () {
+		PolygonCollider2D collider = gameObject.GetComponent<PolygonCollider2D>();
+		if(collider == null){
+			return;
+		}
+		MeshFilter mf = GetComponent<MeshFilter>();
+
+		Vector2[] points = collider.points;
+
+		Triangulator tris = new Triangulator(points);
+
+		Mesh mesh = new Mesh();
+
+		//Lambda expression to convert the vector2 array to vector3 array
+		mesh.vertices = Array.ConvertAll<Vector2,Vector3>(points, vert => new Vector3(vert.x,vert.y,0));
+		mesh.triangles = tris.Triangulate();
+		mf.mesh = mesh;
+
+	}
+}
---
+++ Assets/Editor/Collider2DMeshEditor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Collider2DMesh))]
+public class Collider2DMeshEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		DrawDefaultInspector();
+
+		if(GUILayout.Button("Create Collider Mesh"))
+		{
+			Collider2DMesh cdMesh = (Collider2DMesh)target;
+			Undo.RecordObject(cdMesh.GetComponent<MeshFilter>(), "Create Collider Mesh");
+			cdMesh.TriangulateMesh();
+
+		}
+		if(GUILayout.Button("Delete Collider Mesh"))
+		{
+			Collider2DMesh cdMesh = (Collider2DMesh)target;
+			MeshFilter mf = cdMesh.GetComponent<MeshFilter>();
+			Undo.RecordObject(mf, "Delete Collider Mesh");
+			mf.mesh = null;
+		}
+	}
+}
