433f80a087cc97424d6806a0d6ed3f0afa403730
wrighp <wrightCoder@gmail.com>
2017-10-23 17:08:07 -0400

    :sparkles: Added Resource UI to main scene

--- Assets/Code/UIManager.cs
+++ Assets/Code/UIManager.cs
@@ -8,13 +8,19 @@ public class UIManager : MonoBehaviour {
     private SupplyData data;
 
     void Start(){
-        data = GameObject.FindObjectOfType<SupplyData>();
+        
     }
+
 
 	// Update is called once per frame
 	void Update () {
+		if(data == null){
+			data = GameObject.FindObjectOfType<SupplyData>();
+			return;
+		}
         children[0].text = data.food.ToString();
         children[1].text = data.wood.ToString();
         children[2].text = data.stone.ToString();
+		children[3].text = data.gold.ToString();
     }
 }
