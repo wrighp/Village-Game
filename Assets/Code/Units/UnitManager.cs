e91c834c935ccc79be10c2ac2d9c4556aaf592d6
wrighp <wrightCoder@gmail.com>
2017-11-09 07:19:53 -0500

    :sparkles: Unit Spawning/Manager

--- Assets/Code/Units/UnitManager.cs
+++ Assets/Code/Units/UnitManager.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+public enum UnitAlliance{ FriendlyVillager, FriendlyFighter, EnemyFighter };
 public class UnitManager : NetworkBehaviour {
+	public GameObject unitPrefab;
 
+	public SyncListGameObject friendlyFighters = new SyncListGameObject();
+	public SyncListGameObject friendlyVillagers = new SyncListGameObject();
+	public SyncListGameObject friendlyUnits = new SyncListGameObject();
+	public SyncListGameObject enemyFighters = new SyncListGameObject();
+	public SyncListGameObject allUnits = new SyncListGameObject();
 
+	static public UnitManager i;
+
+	void Awake(){
+		i = this;
+	}
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +33,56 @@ public class UnitManager : NetworkBehaviour {
 	{
 		base.OnStartServer ();
 
+		friendlyFighters.Clear();
+		friendlyVillagers.Clear();
+		friendlyUnits.Clear();
+		enemyFighters.Clear();
+		allUnits.Clear();
+
 	}
 
+	public void SpawnUnit(Vector3 position, UnitAlliance alliance, GameObject prefab = null){
+		Cmds.i.CmdSpawnUnit(position, prefab ?? unitPrefab, alliance);
+	}
+
+	public void returnSpawnUnit(GameObject go, UnitAlliance alliance){
+		switch (alliance) {
+		case UnitAlliance.FriendlyVillager:
+			friendlyVillagers.Add(go);
+			friendlyUnits.Add(go);
+			allUnits.Add(go);
+			break;
+		case UnitAlliance.FriendlyFighter:
+			friendlyFighters.Add(go);
+			friendlyUnits.Add(go);
+			allUnits.Add(go);
+			break;
+		case UnitAlliance.EnemyFighter:
+			enemyFighters.Add(go);
+			allUnits.Add(go);
+			break;
+		default:
+			throw new System.ArgumentOutOfRangeException ();
+		}
+	}
+
+	public void DestroyUnit(GameObject unit){
+		friendlyFighters.Remove(unit);
+		friendlyVillagers.Remove(unit);
+		friendlyUnits.Remove(unit);
+		enemyFighters.Remove(unit);
+		allUnits.Remove(unit);
+	}
+
+}
+
+public partial class Cmds : NetworkBehaviour {
+	[Command]
+	public void CmdSpawnUnit(Vector3 position, GameObject netPrefab, UnitAlliance alliance) {
+		GameObject go = GameObject.Instantiate (netPrefab, position, Quaternion.identity);
+		NetworkServer.Spawn(go);
+
+		UnitManager.i.returnSpawnUnit(go,alliance);
+
+	}
 }
